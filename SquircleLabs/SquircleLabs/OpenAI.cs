0e5b999589a88f48ed0eceac19ba747d838f635c
Timothy Blaisdell <blaisdelltg@gmail.com>
2023-06-27 21:11:39 -0500

    generating usernames via openai.

---
+++ SquircleLabs/SquircleLabs/OpenAI.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Azure;
+using Azure.AI.OpenAI;
+using JSON;
+using static System.Environment;
+
+namespace SquircleLabs {
+    public static class OpenAI {
+        public static string GenerateUsername(bool localhost, bool doPasswordSubs) {
+            try {
+                var folder = Directory.Exists("E:\\web\\timblais\\SquircleLabs") ? "E:\\web\\timblais\\SquircleLabs" : "E:\\SquircleLabs";
+                string path = localhost ? GetFolderPath(SpecialFolder.ApplicationData) : folder;
+                string filepath = Path.Combine(path, "usernames.json");
+                var jarray = new JSONArray();
+                if (File.Exists(filepath)) jarray = new JSONArray(File.ReadAllText(filepath));
+                OpenAIClient client = new(new Uri(_endpoint), new AzureKeyCredential(_key));
+                string prompt = "Generate a zany and whimsical username of 15 alphabetic characters or less for a child using words from Minecraft.";
+                string str = "";
+                do {
+                    var completionsResponse = client.GetCompletions(_engine, prompt);
+                    str = (completionsResponse.Value.Choices[0].Text?.Trim() ?? "").Trim('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+                    str = doPasswordSubs
+                        ? str.Replace("e", "3").Replace("i", "1").Replace("b", "8").Replace("t", "!").Replace("o", "0").Replace("a", "@")
+                        : str;
+                } while (!doPasswordSubs && jarray.Contains(str));
+                if (!doPasswordSubs) {
+                    jarray.put(str);
+                    File.WriteAllText(filepath, jarray.ToString(true, 0));
+                }
+                return str;
+            }
+            catch (Exception ex) {
+                return "Exception: " + ex.Message + NewLine + "url: " + _endpoint + NewLine + "Key starting with: " + _key.Substring(0, 5);
+            }
+        }
+        //public static string GeneratePassword() {
+        //    try {
+        //        OpenAIClient client = new(new Uri(_endpoint), new AzureKeyCredential(_key));
+        //        string prompt = "Generate a zany and whimsical 8 character password for a child using words from the video game franchise Minecraft. Include special characters such as 3 for E, 1 for I, etc..";
+        //        var completionsResponse = client.GetCompletions(_engine, prompt);
+        //        return (completionsResponse.Value.Choices[0].Text?.Trim() ?? "").Trim('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+        //    }
+        //    catch (Exception ex) {
+        //        return "Exception: " + ex.Message + NewLine + "url: " + _endpoint + NewLine + "Key starting with: " + _key.Substring(0, 5);
+        //    }
+        //}
+        public static string MSFoundedAsync() {
+            try {
+                OpenAIClient client = new(new Uri(_endpoint), new AzureKeyCredential(_key));
+                string prompt = "When was Microsoft founded?";
+                var completionsResponse = client.GetCompletions(_engine, prompt);
+                return completionsResponse.Value.Choices[0].Text;
+            }
+            catch (Exception ex) {
+                return "Exception: " + ex.Message + NewLine + "url: " + _endpoint + NewLine + "Key starting with: " + _key.Substring(0, 5);
+            }
+        }
+        private static string _endpoint = "https://squirclesmarts.openai.azure.com/"; //GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+        private static string _engine = "SquircleAIDeploymentTest";
+        private static string _key = "5e4ef018e0224069bcabf7008f8808ab"; //GetEnvironmentVariable("AZURE_OPENAI_KEY");
+    }
+}
--- SquircleLabs/SquircleLabs/Services/WebHooks.asmx.cs
+++ SquircleLabs/SquircleLabs/Services/WebHooks.asmx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Web;
-using System.Web.Script.Serialization;
 using System.Web.Script.Services;
 using System.Web.Services;
 using JSON;
@@ -17,32 +13,11 @@ namespace SquircleLabs.Services {
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
     public class WebHook : WebService {
-        [WebMethod, ScriptMethod(UseHttpGet = false)] 
-        public string HelloWorld() {
-            return "Hello World";
+        [WebMethod, ScriptMethod(UseHttpGet = false)]
+        public string GenerateUsername() {
+            return OpenAI.GenerateUsername(HttpContext.Current.Request.Url.ToString().Contains("localhost"), false);
         }
-        [WebMethod, ScriptMethod(UseHttpGet = false)] 
-        public string RecordEvent() {
-            var folder = Directory.Exists("E:\\web\\timblais\\SquircleLabs") ? "E:\\web\\timblais\\SquircleLabs" : "E:\\SquircleLabs";
-            JSONObject json = null;
-            string contentType = HttpContext.Current.Request.ContentType;
-            if (!contentType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase)) return "NOT JSON";
-            using (Stream stream = HttpContext.Current.Request.InputStream)
-            using (StreamReader reader = new StreamReader(stream)) {
-                stream.Seek(0, SeekOrigin.Begin);
-                string bodyText = reader.ReadToEnd(); bodyText = bodyText == "" ? "{}" : bodyText;
-                json = new JSONObject(bodyText);
-            }
-            bool localhost = HttpContext.Current.Request.Url.ToString().Contains("localhost");
-            string path = localhost ? Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) : folder;
-            string filepath = Path.Combine(path, "events.json");
-            var jarray = new JSONArray();
-            if (File.Exists(filepath)) jarray = new JSONArray(File.ReadAllText(filepath));
-            jarray.put(json);
-            File.WriteAllText(filepath, jarray.ToString(true, 0));
-            return "ok";
-        }
-        [WebMethod, ScriptMethod(UseHttpGet = true)] 
+        [WebMethod, ScriptMethod(UseHttpGet = true)]
         public string GetEvents() {
             var folder = Directory.Exists("E:\\web\\timblais\\SquircleLabs") ? "E:\\web\\timblais\\SquircleLabs" : "E:\\SquircleLabs";
             bool localhost = HttpContext.Current.Request.Url.ToString().Contains("localhost");
@@ -52,7 +27,11 @@ namespace SquircleLabs.Services {
             if (File.Exists(filepath)) jarray = new JSONArray(File.ReadAllText(filepath));
             return jarray.ToString(true, 0);
         }
-        [WebMethod, ScriptMethod(UseHttpGet = true)] 
+        [WebMethod, ScriptMethod(UseHttpGet = false)]
+        public string HelloWorld() {
+            return "Hello World";
+        }
+        [WebMethod, ScriptMethod(UseHttpGet = true)]
         public string PurgeEvents() {
             var folder = Directory.Exists("E:\\web\\timblais\\SquircleLabs") ? "E:\\web\\timblais\\SquircleLabs" : "E:\\SquircleLabs";
             bool localhost = HttpContext.Current.Request.Url.ToString().Contains("localhost");
@@ -60,6 +39,34 @@ namespace SquircleLabs.Services {
             string filepath = Path.Combine(path, "events.json");
             if (File.Exists(filepath)) File.Delete(filepath);
             return "ok";
+        }
+        [WebMethod, ScriptMethod(UseHttpGet = false)]
+        public string RecordEvent() {
+            var folder = Directory.Exists("E:\\web\\timblais\\SquircleLabs") ? "E:\\web\\timblais\\SquircleLabs" : "E:\\SquircleLabs";
+            bool localhost = HttpContext.Current.Request.Url.ToString().Contains("localhost");
+            JSONObject json = null;
+            string contentType = HttpContext.Current.Request.ContentType;
+            if (!contentType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase)) return "NOT JSON";
+            using (Stream stream = HttpContext.Current.Request.InputStream)
+            using (StreamReader reader = new StreamReader(stream)) {
+                stream.Seek(0, SeekOrigin.Begin);
+                string bodyText = reader.ReadToEnd();
+                bodyText = bodyText == "" ? "{}" : bodyText;
+                json = new JSONObject(bodyText);
+                json.put("username", OpenAI.GenerateUsername(localhost, false));
+                json.put("password", OpenAI.GenerateUsername(localhost, true));
+            }
+            string path = localhost ? Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) : folder;
+            string filepath = Path.Combine(path, "events.json");
+            var jarray = new JSONArray();
+            if (File.Exists(filepath)) jarray = new JSONArray(File.ReadAllText(filepath));
+            jarray.put(json);
+            File.WriteAllText(filepath, jarray.ToString(true, 0));
+            return "ok";
+        }
+        [WebMethod, ScriptMethod(UseHttpGet = false)]
+        public string WhenWasMSFounded() {
+            return OpenAI.MSFoundedAsync();
         }
     }
 }
